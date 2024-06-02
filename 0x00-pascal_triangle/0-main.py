196da0549b1a0f7f94ccdff0ff086760e1cfd871
samiunique <yitesam@gmail.com>
2024-06-02 17:09:55 +0300

    do

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -4,7 +4,6 @@
 """
 pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
-
 def print_triangle(triangle):
     """
     Print the triangle
@@ -12,5 +11,6 @@ def print_triangle(triangle):
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
-    if __name__ == "__main__":
-        print_triangle(pascal_triangle(5))
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-''' from typing import List '''
+"""
+This module provides a function to generate Pascal's triangle.
+"""
 
+from typing import List
 
-def pascal_triangle(n: int) -> List[list]:
-    '''
-    Pascal triangle
-    '''
+def pascal_triangle(n: int) -> List[List[int]]:
+    """
+    Generate Pascal's triangle of size `n`.
+
+    Args:
+        n (int): The number of levels in the Pascal's triangle.
+
+    Returns:
+        List[List[int]]: A list of lists, where each sublist represents a level in the Pascal's triangle.
+
+    Example:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    """
     if n <= 0:
         return []
 
@@ -19,9 +32,9 @@ def pascal_triangle(n: int) -> List[list]:
 
     for i in range(2, n):
         temp = [1, 1]
-        for j in range(0, len(triangle[-1])-1):
+        for j in range(len(triangle[-1]) - 1):
             a = triangle[-1][j]
-            b = triangle[-1][j+1]
+            b = triangle[-1][j + 1]
             temp.insert(-1, a + b)
         triangle.append(temp)
 
